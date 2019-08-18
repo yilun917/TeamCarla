642efdb720bc16aef743d8ee66c21f009c6f92c9
Greg Laughlin <gregoryjlaughlin@gmail.com>
2019-08-18 17:28:44 -0500

    Got car running around test lot but it runs out of waypoints.

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -24,7 +24,7 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+LOOKAHEAD_WPS = 100 #200 Number of waypoints we will publish. You can change this number
 MAX_DECEL = 0.5 # the max breaking deceleration m/s^2
 
 class WaypointUpdater(object):
