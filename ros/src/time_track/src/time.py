db6eb9bf85fb74a7a8ee91ca5d4a10aa1545dfaa
Yilun Yu <sinyyl11@gmail.com>
2019-08-19 01:03:30 -0700

    working version with 3s halt at begging

--- ros/src/time_track/src/time.py
+++ ros/src/time_track/src/time.py
@@ -3,7 +3,37 @@
 import rospy
 from std_msgs.msg import Int32, Bool
 
+def loop():
+    global my_pub
+    global initilizing
+    rate = rospy.Rate(50)
+    while not rospy.is_shutdown():
+        my_pub.publish(initilizing)
+        if(initilizing.data):
+            initilizing.data = False
+        rate.sleep()
 
+def time_cb(msg):
+    # if there is any message in topic "traffic_waypoint" and is first time
+    # pass a False value to let the control node halt for 3 s
+    global initilizing
+    global first
+    if(first):
+        initilizing.data = True
+        first = False
+
+rospy.init_node("time_node")
+initilizing = Bool()
+initilizing.data = False
+first = True
+my_pub = rospy.Publisher("/time_track", Bool, queue_size=1)
+my_sub = rospy.Subscriber("/traffic_waypoint", Int32, time_cb)
+loop()
+
+
+
+
+'''
 class TimeTracker():
     def __init__(self):
         rospy.init_node("time_node")
@@ -28,7 +58,7 @@ class TimeTracker():
 if __name__ == 'main':
     print("///////////////////////////////////////////////////////////////////")
     TimeTracker()
-
+'''
    
 
 
